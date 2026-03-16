8a2f3a8624717fbbfc06fb1399424d01e0e0baef
LatorreSony <latorretorras@gmail.com>
2026-03-16 17:53:43 +0100

    Initial Commit

---
+++ FileConverter/FileConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileConverterApp.swift
+//  FileConverter
+//
+//  Created by Alex Latorre on 16/3/26.
+//
+
+import SwiftUI
+
+@main
+struct FileConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
